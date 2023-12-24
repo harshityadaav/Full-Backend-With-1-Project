0738e2c47379447188a5e1265ba262314f14faac
harshityadaav <harshit2622002@gmail.com>
2023-12-24 21:30:32 +0530

    login logout & update

--- src/app.js
+++ src/app.js
@@ -6,13 +6,14 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true 
+    credentials: true
 }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
 
 //routes import
 import userRouter from './routes/user.routes.js'
@@ -23,4 +24,4 @@ app.use("/api/v1/users", userRouter)
 
 // http://localhost:8000/api/v1/users/register
 
-export {app}
+export { app }
